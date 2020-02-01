7e1107de353383cf5a0ffe876fb77f0227d17cc4
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-01 14:44:20 -0700

    wired up post and todo buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <div>App</div>;
+export class App extends Component {
+  state = { resource: "posts" };
+
+  onResourceChange = resource => {
+    this.setState({ resource });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div style={{ marginTop: "1rem" }}>
+          <button
+            onClick={() => this.onResourceChange("posts")}
+            className="ui button primary"
+          >
+            Posts
+          </button>
+          <button
+            onClick={() => this.onResourceChange("todos")}
+            className="ui button red"
+          >
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
+  }
 }
 
 export default App;
